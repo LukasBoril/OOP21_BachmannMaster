e583f68a01663d4d82a7445cd68b612b255d1ffc
bacn <mbachmann@trdi.io>
2021-04-09 16:10:06 +0200

    initial of branch

--- createbranches.sh
+++ createbranches.sh
@@ -10,25 +10,7 @@ createbranch () {
   git checkout develop
 }
 
-createbranch bajrami_kaltrim
-createbranch berchtold_thomas
-createbranch boban_pascal
-createbranch boril_lukas
-createbranch dilber_danijel
-createbranch duss_nadine
-createbranch gasser_matthias
-createbranch giger_thomas
-createbranch isler_oliver
-createbranch nguyen_phat
-createbranch popov_marjan
-createbranch schmidt_patrick
-createbranch stadelmann_daniel
-createbranch widmer_sidney
-
-
-
-
-
+createbranch haller_stefan
 
 
 
