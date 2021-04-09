e76e22683fb01ee5402e78a8713dff6dd148f7a3
bacn <mbachmann@trdi.io>
2021-04-09 16:11:05 +0200

    add stefan haller

--- createbranches.sh
+++ createbranches.sh
@@ -13,4 +13,23 @@ createbranch () {
 createbranch haller_stefan
 
 
+createbranch bajrami_kaltrim
+createbranch berchtold_thomas
+createbranch boban_pascal
+createbranch boril_lukas
+createbranch dilber_danijel
+createbranch duss_nadine
+createbranch gasser_matthias
+createbranch giger_thomas
+createbranch isler_oliver
+createbranch nguyen_phat
+createbranch popov_marjan
+createbranch schmidt_patrick
+createbranch stadelmann_daniel
+createbranch widmer_sidney
+
+
+
+
+
 
