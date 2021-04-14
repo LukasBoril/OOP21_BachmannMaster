bec3af8a19b0cad06505db5d58c3e7a04be22370
bonux <pascal.boban@bluewin.ch>
2021-04-14 21:32:07 +0200

    task 3a added anonyme innere Klasse

---
+++ src/main/java/ch/zhaw/homework/le01/task03_lambda/AdditionalPaneLambda.java
@@ -0,0 +1,64 @@
+package ch.zhaw.homework.le01.task03_lambda;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class AdditionalPaneLambda extends Application {
+
+    public void start(Stage primaryStage) {
+        Label l1 = new Label("Hallo");
+        Label l2 = new Label("*bo*");
+        VBox root = new VBox();
+        root.getChildren().add(l1);
+        root.getChildren().add(l2);
+        root.getChildren().add(createButton());
+        root.getChildren().add(createButtonLambda());
+
+        Scene scene = new Scene(root, 240, 140);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Hello user");
+        primaryStage.show();
+    }
+
+    Pane createButton() {
+        final Button btn = new Button();
+        btn.setText("Add <Hello Bo> Label");
+        final FlowPane pane = new FlowPane();
+        pane.setPadding(new Insets(7, 7, 7, 7));
+        pane.getChildren().add(btn);
+
+        // ActionHandler registrieren
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                pane.getChildren().add(new Label("- Anonyme Bo! -"));
+            }
+        });
+        return pane;
+    }
+
+    Pane createButtonLambda() {
+        final Button btn = new Button();
+        btn.setText("Add <Hello Bo> Label");
+        final FlowPane pane = new FlowPane();
+        pane.setPadding(new Insets(7, 7, 7, 7));
+        pane.getChildren().add(btn);
+        // ActionHandler registrieren
+        btn.setOnAction(event -> pane.getChildren()
+                .add(new Label("- Lambda Bo! -")));
+        return pane;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
