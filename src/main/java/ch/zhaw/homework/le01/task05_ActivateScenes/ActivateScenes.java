ac16030f7f48f5d50b255769676b1765ec512a71
bonux <pascal.boban@bluewin.ch>
2021-04-14 23:26:06 +0200

    Activate Scenes created and changed to lambda

--- src/main/java/ch/zhaw/homework/le01/task05_ActivateScenes/ActivateScenes.java
+++ src/main/java/ch/zhaw/homework/le01/task05_ActivateScenes/ActivateScenes.java
@@ -3,14 +3,10 @@ package ch.zhaw.homework.le01.task05_ActivateScenes;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class ActivateScenes extends Application  {
