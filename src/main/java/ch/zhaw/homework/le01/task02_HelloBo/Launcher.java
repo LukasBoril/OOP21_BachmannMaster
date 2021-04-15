79104617b386e840cf287cd0b0711bdc375931a1
bonux <pascal.boban@bluewin.ch>
2021-04-16 00:11:51 +0200

    lets see how it works in the future. Not final implemented

---
+++ src/main/java/ch/zhaw/homework/le01/task02_HelloBo/Launcher.java
@@ -0,0 +1,9 @@
+package ch.zhaw.homework.le01.task02_HelloBo;
+
+public class Launcher {
+
+   /*public static void main(String[] args) {
+           HelloBo helloBo = new HelloBo();
+           helloBo.launch();
+   }*/
+}
