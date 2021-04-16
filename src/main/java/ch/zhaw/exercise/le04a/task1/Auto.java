65c1f22b73bb08ec3bcfb7cecbe75638ba2162d8
Kaltrim Bajrami <bajrakal@students.zhaw.ch>
2021-04-16 19:54:00 +0200

    Test Aufgabe HelloWorld

---
+++ src/main/java/ch/zhaw/exercise/le04a/task1/Auto.java
@@ -0,0 +1,25 @@
+package ch.zhaw.exercise.le04a.task1;
+
+import java.io.Serializable;
+
+public class Auto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private String farbe;
+    private Motor motor;
+
+    public Auto(String farbe, Motor motor) {
+        this.farbe = farbe;
+        this.motor = motor;
+
+    }
+
+
+    public String getFarbe() {
+        return farbe;
+    }
+
+    public Motor getMotor() {
+        return motor;
+    }
+}
