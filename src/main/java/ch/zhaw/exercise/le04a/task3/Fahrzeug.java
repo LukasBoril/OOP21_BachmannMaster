2de96b90de6a27bdd5f871b7236474f98337a616
Kaltrim Bajrami <bajrakal@students.zhaw.ch>
2021-04-16 20:10:46 +0200

    Test Aufgabe HelloWorld

---
+++ src/main/java/ch/zhaw/exercise/le04a/task3/Fahrzeug.java
@@ -0,0 +1,9 @@
+package ch.zhaw.exercise.le04a.task3;
+
+import java.io.Serializable;
+
+public class Fahrzeug implements Serializable {
+
+
+
+}
---
+++ src/main/java/ch/zhaw/exercise/le04a/task3/Motor.java
@@ -0,0 +1,18 @@
+package ch.zhaw.exercise.le04a.task3;
+
+import java.io.Serializable;
+
+public class Motor implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private int hubraum;
+
+    public Motor(int hubraum) {
+        this.hubraum = hubraum;
+    }
+
+    public int getHubraum() {
+        return hubraum;
+    }
+
+}
