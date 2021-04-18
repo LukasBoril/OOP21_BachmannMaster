ca9af4d1378f28ad468133d9f290c4ad18cba96e
bobanpas <bobanpas@students.zhaw.ch>
2021-04-18 10:43:06 +0200

    old louncher deletet
    not needed anymore

--- src/main/java/ch/zhaw/homework/le01/task02_HelloBo/Launcher.java
+++
@@ -1,9 +0,0 @@
-package ch.zhaw.homework.le01.task02_HelloBo;
-
-public class Launcher {
-
-   /*public static void main(String[] args) {
-           HelloBo helloBo = new HelloBo();
-           helloBo.launch();
-   }*/
-}
