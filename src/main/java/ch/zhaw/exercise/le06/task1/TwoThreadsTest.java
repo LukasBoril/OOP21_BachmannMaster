e21f6724def7a475d45207a5cd5434a9e059b03a
Kaltrim Bajrami <bajrakal@students.zhaw.ch>
2021-04-23 16:22:57 +0200

    exercises le06

--- src/main/java/ch/zhaw/exercise/le06/task1/TwoThreadsTest.java
+++ src/main/java/ch/zhaw/exercise/le06/task1/TwoThreadsTest.java
@@ -1,14 +1,7 @@
 package ch.zhaw.exercise.le06.task1;
 
 public class TwoThreadsTest {
-<<<<<<< HEAD
-    public static void main(String[] args) {
-        new SimpleThread("Jamaica").start();
-        new SimpleThread("Fiji").start();
-        System.out.println("main exits " + Thread.currentThread());
-    }
-}
-=======
+
 
     public static void main(String[] args) {
         new SimpleThread("Jamaica").start();
@@ -18,15 +11,11 @@ public class TwoThreadsTest {
 
 }
 
->>>>>>> master
 class SimpleThread extends Thread {
     public SimpleThread(String str) {
         super(str);
     }
-<<<<<<< HEAD
-=======
 
->>>>>>> master
     public void run() {
         for (int i = 0; i < 10; i++) {
             System.out.println(i + " " + getName());
