5fd901d72ecdc26568f537914d3825777400cd27
Thomas Berchtold <thomas.berchtold@id.ethz.ch>
2021-04-23 12:04:56 +0000

    Updated

---
+++ src/main/java/ch/orbnet/lab/SomeIDETests.java
@@ -0,0 +1,9 @@
+package ch.orbnet.lab;
+
+public class SomeIDETests {
+
+    public static void main( public static void main(String[] args){){
+        System.out.println("We are here");
+    }
+
+}
